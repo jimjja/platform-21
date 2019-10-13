a982fcb53a2eab685404f0db61203897eea36464
Jemal Ahmedov <jahmedov@jemals-mbp.home>
2019-10-14 00:11:35 +0100

    add offsets

--- src/api.js
+++ src/api.js
@@ -7,6 +7,15 @@ const appId = '0132e52f';
 const dateFormat = 'YYYY-MM-DD';
 const timeFormat = 'HH:mm';
 
+function getOffsetTime(dateTime) {
+  const noDigits = '2-digit';
+  const options = {
+    hour: noDigits, minute: noDigits, second: noDigits,
+  };
+  const date = new Date(dateTime);
+  return date.toLocaleTimeString('en-gb', options);
+}
+
 async function getTrains(date, time) {
   if (!date || !time) {
     throw new Error('Date and time are missing');
@@ -15,11 +24,23 @@ async function getTrains(date, time) {
     const formatedDate = moment(date).format(dateFormat);
     const formatedTime = moment(time).format(timeFormat);
 
+    const stationCode = 'CHX';
+    const fromOffset = getOffsetTime(new Date('10-10-2019 00:15:00'));
+    const toOffset = getOffsetTime(new Date('10-10-2019 00:45:00'));
+
+    const queryString = [
+      `app_id=${appId}`,
+      `app_key=${appKey}`,
+      'train_status=passenger',
+      `from_offset=PT${fromOffset}`,
+      `to_offset=PT${toOffset}`,
+    ];
+
     const result = await axios.get(
-      `https://transportapi.com/v3/uk/train/station/CHX/${formatedDate}/${formatedTime}/timetable.json?app_id=${appId}&app_key=${appKey}&train_status=passenger`,
+      `https://transportapi.com/v3/uk/train/station/${stationCode}/${formatedDate}/${formatedTime}/timetable.json?${queryString.join('&')}`,
     );
     console.log(result.data.departures);
-    return result.data && result.data.departures && result.data.departures.all || [];
+    return (result.data && result.data.departures && result.data.departures.all) || [];
   } catch (error) {
     console.log(error);
     throw error;
