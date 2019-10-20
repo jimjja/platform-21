518bde48af75fa21127386db211a0cdf604a6466
Jemal Ahmedov <jahmedov@jemals-mbp.home>
2019-10-20 21:02:30 +0100

    Updates

--- precache-manifest.6302b98d9e3aaf687b10b71679231cc8.js
+++ precache-manifest.6302b98d9e3aaf687b10b71679231cc8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a1def89223d8072586028ad7b49a561",
+    "revision": "a8036814d78f97d02526366beaec30bb",
     "url": "/platform-21/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/platform-21/static/css/2.ee51aa88.chunk.css"
   },
   {
-    "revision": "4a62b235e43000205058",
-    "url": "/platform-21/static/css/main.badf6db8.chunk.css"
+    "revision": "1f2f97182cedeef378f6",
+    "url": "/platform-21/static/css/main.6bb0528f.chunk.css"
   },
   {
     "revision": "43d1ecd30b6494171c14",
     "url": "/platform-21/static/js/2.7ef35f71.chunk.js"
   },
   {
-    "revision": "4a62b235e43000205058",
+    "revision": "1f2f97182cedeef378f6",
     "url": "/platform-21/static/js/main.fac87ee0.chunk.js"
   },
   {
